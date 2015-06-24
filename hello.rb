32fa3445782dcd47bce6521bde3d8f3574262408
Christian Miller <cwgmiller@gmail.com>
2015-06-24 17:41:20 +0100

    welcome page added

--- hello.rb
+++ hello.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 
 get '/' do
+	@name = %w(Amigo Oscar Viking).sample
 	erb :index
 end
 
@@ -10,4 +11,9 @@ end
 
 get '/shotgun' do 
 	"Testing shotgun"
+end
+
+get '/hello' do 
+	@visitor = params[:name]
+	erb :index
 end
